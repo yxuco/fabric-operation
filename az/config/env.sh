9ef0954337db83affc9e75c883bbe07e16913ddf
Yueming Xu <yxucolo@gmail.com>
2019-11-06 11:01:54 -0700

    remove autogen config

--- az/config/env.sh
+++
@@ -1,4 +0,0 @@
-export STORAGE_ACCT=fabstore
-export STORAGE_SHARE=fabshare
-export SMB_PATH=//fabstore.file.core.windows.net/fabshare
-export STORAGE_KEY=/c/uaM+/PV/YZCpLedK7Z9Iw27klxEIQRYXYsh714wquZJiiFe1EhIu62WKJp8phdC4C7j0Lz8Mz2FabVvtkew==
